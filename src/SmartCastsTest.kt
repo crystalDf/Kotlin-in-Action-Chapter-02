a9320816b4ea394d30d92c396de452fec9cb0077
crystalDf <chendong333@gmail.com>
2018-06-16 16:59:34 +0800

    Chapter 02

--- src/SmartCastsTest.kt
+++ src/SmartCastsTest.kt
@@ -1,6 +1,6 @@
 interface Expr
-class Num(val value: Int): Expr
-class Sum(val left: Expr, val right: Expr): Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
 
 fun eval(e: Expr): Int {
     if (e is Num) {
